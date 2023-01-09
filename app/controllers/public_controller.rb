72a24d7aebcba0caf260c6ea2d3962e4f393cb12
Chris Tiangco <quark0x@yahoo.com>
2023-01-09 12:46:04 -0700

    test end-to-end update of sealed secrets k8s manifests

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,6 +2,6 @@
 
 class PublicController < ApplicationController
   def index
-    render html: '<h1>Hello there! This is so awesome!!! I deployed on 01/08/2023 @ 10:13PM</h1>'
+    render html: '<h1>Hello there! This is so awesome!!! I deployed on 01/09/2023 @ 12:44PM</h1>'
   end
 end
