84954e5f6735b794bee115b7ae41f27071631877
Chris Tiangco <quark0x@yahoo.com>
2023-01-22 14:02:16 -0700

    e2e deployment test

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,6 +2,6 @@
 
 class PublicController < ApplicationController
   def index
-    render html: '<h1>Hello there! This is so awesome!!! I deployed on 01/15/2023 @ 2:12AM</h1>'
+    render html: '<h1>Hello there! This is so awesome!!! I deployed on 01/22/2023 @ 2:01PM</h1>'
   end
 end
